ccace757d2af0c0006a31ec6b01527e0ebb27b4c
unknown <hafidamroucheg@gmail.com>
2024-09-05 07:36:54 +0100

    'commit'

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Store, StateShippingCost, Status, Visitor, IpAddress, GSInfo, VIPStore
+from .models import Store, StateShippingCost, Status, Visitor, IpAddress, GSInfo, VIPStore, Domain
 # Register your models here.
 
 admin.site.register(Store)
@@ -9,5 +9,5 @@ admin.site.register(Visitor)
 admin.site.register(IpAddress)
 admin.site.register(GSInfo)
 admin.site.register(VIPStore)
-
+admin.site.register(Domain)
 
