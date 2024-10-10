68530fba09384ac08fdab180e0bd2ab383dc1967
unknown <hafidamroucheg@gmail.com>
2024-10-10 14:14:12 +0100

    'commit'

--- product/serialiers.py
+++ product/serialiers.py
@@ -2,14 +2,20 @@ from rest_framework import serializers
 from .models import Product
 
 class SearchedProductTypeASerializer(serializers.ModelSerializer):
+    product_id = serializers.SerializerMethodField(read_only=True)
+    def get_product_id(self, obj):
+        return obj.id
     class Meta:
         model = Product
-        fields = ['id', 'image', 'title']
+        fields = ['product_id', 'image', 'title']
 
 class SearchedProductDetailedSerializer(serializers.ModelSerializer):
+    product_id = serializers.SerializerMethodField(read_only=True)
+    def get_product_id(self, obj):
+        return obj.id
     class Meta:
         model = Product
-        fields = ['id', 'image', 'title', 'price', 'active', 'views', 'quantity', 'slug']
+        fields = ['product_id', 'image', 'title', 'price', 'active', 'views', 'quantity', 'slug']
 
 class ProductSerializerA(serializers.ModelSerializer):
     product_id = serializers.SerializerMethodField(read_only=True)
